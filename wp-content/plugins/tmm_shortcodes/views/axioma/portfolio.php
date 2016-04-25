d20c19cb39796c6423a3a8f0c5fd2dc5f02849a1
bourroush <boris@godynamo.com>
2016-04-25 10:56:38 +0200

    Change triggered link on homepahe for Principals represented
    Fix CSS issue
    Add home class on french homepage

--- wp-content/plugins/tmm_shortcodes/views/axioma/portfolio.php
+++ wp-content/plugins/tmm_shortcodes/views/axioma/portfolio.php
@@ -5,53 +5,53 @@ TMM_Functions::enqueue_script('isotope');
 //***
 $layout = $content;
 if (!$posts_per_page) {
-	$posts_per_page = 6;
+  $posts_per_page = 6;
 }
 //***
 $tax_query_array = array();
 //***
 if (!isset($skills)) {
-	$skills = 'all';
+  $skills = 'all';
 }
 
 if (!isset($clients)) {
-	$clients = 'all';
+  $clients = 'all';
 }
 
 if (!isset($tags)) {
-	$tags = '';
+  $tags = '';
 }
 //***
 if ($skills != 'all') {
-	$tax_query_array[] = array(
-		'taxonomy' => 'skills',
-		'field' => 'term_id',
-		'terms' => array($skills),
-	);
+  $tax_query_array[] = array(
+    'taxonomy' => 'skills',
+    'field' => 'term_id',
+    'terms' => array($skills),
+  );
 }
 
 if ($clients != 'all') {
-	$tax_query_array[] = array(
-		'taxonomy' => 'clients',
-		'field' => 'term_id',
-		'terms' => array($clients),
-	);
+  $tax_query_array[] = array(
+    'taxonomy' => 'clients',
+    'field' => 'term_id',
+    'terms' => array($clients),
+  );
 }
 $folio_page = 1;
 if (isset($_GET['folio_page'])) {
-	$folio_page = $_GET['folio_page'];
+  $folio_page = $_GET['folio_page'];
 }
 global $post;
 $current_page_id = $post->ID;
 //***
 $w_query = new WP_Query();
 $query = $w_query->query(array(
-	'tag' => $tags,
-	'tax_query' => $tax_query_array,
-	'post_type' => TMM_Portfolio::$slug,
-	'posts_per_page' => $posts_per_page,
-	'paged' => $folio_page
-		)
+  'tag' => $tags,
+  'tax_query' => $tax_query_array,
+  'post_type' => TMM_Portfolio::$slug,
+  'posts_per_page' => $posts_per_page,
+  'paged' => $folio_page
+    )
 );
 
 
@@ -59,170 +59,170 @@ $query = $w_query->query(array(
 $article_css_class = "";
 $featured_image_alias = "460*350";
 switch ($layout) {
-	case 2:
-		$article_css_class = "eight columns";
-		break;
-	case 3:
-		$article_css_class = "one-third column";
-		break;
-	case 4:
-		$article_css_class = "four columns";
-		break;
-	default:
-		break;
+  case 2:
+    $article_css_class = "eight columns";
+    break;
+  case 3:
+    $article_css_class = "one-third column";
+    break;
+  case 4:
+    $article_css_class = "four columns";
+    break;
+  default:
+    break;
 }
 ?>
 
 <div class="filter-holder clearfix">
 
-	<?php if ($layout != 1): ?>
+  <?php if ($layout != 1): ?>
 
-		<ul id="portfolio-filter" class="portfolio-filter clearfix">
+    <ul id="portfolio-filter" class="portfolio-filter clearfix">
 
-			<?php
-			$folio_tags = array();
-			$posts_tags = array();
-			foreach ($query as $p) {
-				$tmp = wp_get_post_tags($p->ID);
-				foreach ($tmp as $tag_object) {
-					$folio_tags[$tag_object->term_id] = $tag_object;
-					$posts_tags[$p->ID][] = $tag_object;
-				}
-			}
-			?>
-			<li><a data-categories="*" class=""><?php _e('All', 'tmm_shortcodes'); ?></a></li>
+      <?php
+      $folio_tags = array();
+      $posts_tags = array();
+      foreach ($query as $p) {
+        $tmp = wp_get_post_tags($p->ID);
+        foreach ($tmp as $tag_object) {
+          $folio_tags[$tag_object->term_id] = $tag_object;
+          $posts_tags[$p->ID][] = $tag_object;
+        }
+      }
+      ?>
+      <li><a data-categories="*" class=""><?php _e('All', 'tmm_shortcodes'); ?></a></li>
 
-			<?php if (!empty($folio_tags)): ?>
-				<?php foreach ($folio_tags as $term_id => $tag) : ?>
-					<li><a data-categories="<?php echo $term_id ?>"><?php echo $tag->name ?></a></li>
-				<?php endforeach; ?>
-			<?php endif; ?>
+      <?php if (!empty($folio_tags)): ?>
+        <?php foreach ($folio_tags as $term_id => $tag) : ?>
+          <li><a data-categories="<?php echo $term_id ?>"><?php echo $tag->name ?></a></li>
+        <?php endforeach; ?>
+      <?php endif; ?>
 
-		</ul><!--/ .portfolio-filter-->
+    </ul><!--/ .portfolio-filter-->
 
-	<?php endif; ?>
+  <?php endif; ?>
 
-	<section id="portfolio-items" class="portfolio-items clearfix">
+  <section id="portfolio-items" class="portfolio-items clearfix">
 
-		<?php foreach ($query as $post) : ?>
+    <?php foreach ($query as $post) : ?>
 
-			<?php
-			$tag_css_class = "";
+      <?php
+      $tag_css_class = "";
 
-			if (isset($posts_tags[$post->ID])) {
-				$tags = $posts_tags[$post->ID];
-				if (!empty($tags)) {
-					foreach ($tags as $key => $tag) {
-						if ($key > 0) {
-							$tag_css_class .= " ";
-						}
-						$tag_css_class .= $tag->term_id;
-					}
-				}
-			}
-			?>
+      if (isset($posts_tags[$post->ID])) {
+        $tags = $posts_tags[$post->ID];
+        if (!empty($tags)) {
+          foreach ($tags as $key => $tag) {
+            if ($key > 0) {
+              $tag_css_class .= " ";
+            }
+            $tag_css_class .= $tag->term_id;
+          }
+        }
+      }
+      ?>
 
-			<article id="post-<?php the_ID(); ?>" <?php post_class($article_css_class . ' '); ?> data-categories="<?php echo $tag_css_class ?>">
+      <article id="post-<?php the_ID(); ?>" <?php post_class($article_css_class . ' '); ?> data-categories="<?php echo $tag_css_class ?>">
 
-				<div class="work-item">
+        <div class="work-item">
 
-					<?php
+          <?php
 
-						switch ($layout) {
-							case 2:
-								$disable_icons = TMM::get_option('folio_disable_icons_2col');
-								break;
-							case 3:
-								$disable_icons = TMM::get_option('folio_disable_icons_3col');
-								break;
-							case 4:
-								$disable_icons = TMM::get_option('folio_disable_icons_4col');
-								break;
-							default:
-								break;
-						}
+            switch ($layout) {
+              case 2:
+                $disable_icons = TMM::get_option('folio_disable_icons_2col');
+                break;
+              case 3:
+                $disable_icons = TMM::get_option('folio_disable_icons_3col');
+                break;
+              case 4:
+                $disable_icons = TMM::get_option('folio_disable_icons_4col');
+                break;
+              default:
+                break;
+            }
 
-					?>
+          ?>
 
-					<?php if ($disable_icons): ?>
+          <?php if ($disable_icons): ?>
 
-						<a href="<?php echo $post->guid ?>" class="single-image full-link">
-							<img src="<?php echo TMM_Helper::get_post_featured_image($post->ID, $featured_image_alias); ?>" alt="<?php the_title(); ?>" />
-						</a>
+            <a href="<?php echo $post->guid ?>" class="single-image full-link">
+              <img src="<?php echo TMM_Helper::get_post_featured_image($post->ID, $featured_image_alias); ?>" alt="<?php the_title(); ?>" />
+            </a>
 
-					<?php else: ?>
+          <?php else: ?>
 
-						<img src="<?php echo TMM_Helper::get_post_featured_image($post->ID, $featured_image_alias); ?>" alt="<?php the_title(); ?>" />
+            <img src="<?php echo TMM_Helper::get_post_featured_image($post->ID, $featured_image_alias); ?>" alt="<?php the_title(); ?>" />
 
-						<div class="image-extra">
+            <div class="image-extra">
 
-							<div class="extra-content">
+              <div class="extra-content">
 
-								<div class="inner-extra">
+                <div class="inner-extra">
 
-									<a class="single-image link-icon" href="<?php  echo get_permalink($post->ID); ?>">Permalink</a>
-									<a class="single-image plus-icon" data-fancybox-group="gallery" href="<?php echo TMM_Helper::get_post_featured_image($post->ID, ''); ?>">Image</a>
+                  <a class="single-image link-icon" href="<?php  echo get_permalink($post->ID); ?>">Permalink</a>
+                  <a class="single-image plus-icon" data-fancybox-group="gallery" href="<?php echo TMM_Helper::get_post_featured_image($post->ID, ''); ?>">Image</a>
 
-									<h4 class="extra-title"><?php echo $post->post_title ?></h4>
-									<?php
-									if (!isset($show_categories)) {
-										$show_categories = 1;
-									}
-									?>
-									<?php if ($show_categories): ?>
-										<span class="extra-category">
-											<?php
-											$tags = wp_get_post_tags($post->ID);
-											foreach ($tags as $key => $value) {
-												if ($key > 0) {
-													echo ' / ';
-												}
-												echo $value->name;
-											}
-											?>
-										</span><!--/ .extra-category-->
-									<?php endif; ?>
+                  <h4 class="extra-title"><?php echo $post->post_title ?></h4>
+                  <?php
+                  if (!isset($show_categories)) {
+                    $show_categories = 1;
+                  }
+                  ?>
+                  <?php if ($show_categories): ?>
+                    <span class="extra-category">
+                      <?php
+                      $tags = wp_get_post_tags($post->ID);
+                      foreach ($tags as $key => $value) {
+                        if ($key > 0) {
+                          echo ' / ';
+                        }
+                        echo $value->name;
+                      }
+                      ?>
+                    </span><!--/ .extra-category-->
+                  <?php endif; ?>
 
-								</div><!--/ .inner-extra-->
+                </div><!--/ .inner-extra-->
 
-							</div><!--/ .extra-content-->
+              </div><!--/ .extra-content-->
 
-						</div><!--/ .image-extra-->
+            </div><!--/ .image-extra-->
 
-					<?php endif; ?>
+          <?php endif; ?>
 
-				</div><!--/ .work-item-->
+        </div><!--/ .work-item-->
 
-			</article><!--/ .project-item-->
+      </article><!--/ .project-item-->
 
-			<?php
-		endforeach;
-		?>
+      <?php
+    endforeach;
+    ?>
 
-	</section><!--/ .portfolio-items-->
+  </section><!--/ .portfolio-items-->
 
 </div><!--/ .filter-holder-->
 
 <?php if ($w_query->max_num_pages > 1): ?>
-	<div class="wp-pagenavi">
+  <div class="wp-pagenavi">
 
-		<?php if ($folio_page - 1 > 0): ?>
-			<a href="<?php echo get_permalink($current_page_id) ?>?folio_page=<?php echo($folio_page - 1) ?>" class="prev page-numbers"></a>
-		<?php endif; ?>
+    <?php if ($folio_page - 1 > 0): ?>
+      <a href="<?php echo get_permalink($current_page_id) ?>?folio_page=<?php echo($folio_page - 1) ?>" class="prev page-numbers"></a>
+    <?php endif; ?>
 
-		<?php for ($i = 0; $i < $w_query->max_num_pages; $i++): ?>
+    <?php for ($i = 0; $i < $w_query->max_num_pages; $i++): ?>
 
-			<?php if ($folio_page == ($i + 1)): ?>
-				<span class="page-numbers current"><?php echo($i + 1) ?></span>
-			<?php else: ?>
-				<a href="<?php echo get_permalink($current_page_id) ?>?folio_page=<?php echo($i + 1) ?>" class="page-numbers"><?php echo ($i + 1) ?></a>
-			<?php endif; ?>
+      <?php if ($folio_page == ($i + 1)): ?>
+        <span class="page-numbers current"><?php echo($i + 1) ?></span>
+      <?php else: ?>
+        <a href="<?php echo get_permalink($current_page_id) ?>?folio_page=<?php echo($i + 1) ?>" class="page-numbers"><?php echo ($i + 1) ?></a>
+      <?php endif; ?>
 
-		<?php endfor; ?>
+    <?php endfor; ?>
 
-		<?php if ($folio_page < $w_query->max_num_pages): ?>
-			<a href="<?php echo get_permalink($current_page_id) ?>?folio_page=<?php echo($folio_page + 1) ?>" class="next page-numbers"></a>
-		<?php endif; ?>
+    <?php if ($folio_page < $w_query->max_num_pages): ?>
+      <a href="<?php echo get_permalink($current_page_id) ?>?folio_page=<?php echo($folio_page + 1) ?>" class="next page-numbers"></a>
+    <?php endif; ?>
 
-	</div>
+  </div>
 <?php endif; ?>
